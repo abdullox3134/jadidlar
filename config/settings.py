4f45677ea8af8752f928e0dceecefa8b50ecd2fd
abdullox <git@github.com:abdullox3134/jadidlar.git>
2024-02-22 20:14:30 +0500

    settings psql

--- config/settings.py
+++ config/settings.py
@@ -105,7 +105,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'jadid_db',
         'USER': 'jadidlar_user',
         'PASSWORD': 'jadidlar_password',
