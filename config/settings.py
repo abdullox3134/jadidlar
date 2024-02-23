83685f46eb6b8f73c9c743bf65bb466bd8e65c44
abdullox <git@github.com:abdullox3134/jadidlar.git>
2024-02-23 19:42:39 +0500

    token and like

--- config/settings.py
+++ config/settings.py
@@ -103,22 +103,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'jadid_db',
-#         'USER': 'jadidlar_user',
-#         'PASSWORD': 'jadidlar_password',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jadid_db',
+        'USER': 'jadidlar_user',
+        'PASSWORD': 'jadidlar_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
