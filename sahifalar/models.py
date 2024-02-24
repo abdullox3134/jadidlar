1a08c6b56cae5fb0226fd81702f0ab2e8ef81303
abdullox <git@github.com:abdullox3134/jadidlar.git>
2024-02-24 18:20:00 +0500

    videos title edit

--- sahifalar/models.py
+++ sahifalar/models.py
@@ -6,7 +6,7 @@ class Sahifalar(models.Model):
     title = models.CharField(max_length=255, verbose_name='Sahifa nomi', blank=True, null=True)
     text = RichTextField(verbose_name='Matn', blank=True, null=True)
     image = models.ImageField(upload_to='images/', null=True, blank=True)
-    file = models.FileField(upload_to='files/', verbose_name='Fayl')
+    file = models.FileField(upload_to='files/', verbose_name='Fayl', blank=True, null=True)
     count = models.BigIntegerField(null=True, blank=True, default=0)
     create = models.DateTimeField(auto_now_add=True, verbose_name='Yaratilgan vaqti')
     update = models.DateTimeField(auto_now=True, verbose_name='Yangilangan vaqti')
