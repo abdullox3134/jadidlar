eb54d2f808f17b4114b26543bab50d35b524275e
abdullox <abdulloxislomov39@gmail.com>
2024-06-19 20:40:56 +0500

    static

--- config/settings.py
+++ config/settings.py
@@ -176,7 +176,7 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
